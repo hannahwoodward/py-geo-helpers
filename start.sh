1d0d2b5164e4d7b7768dc7c6e574e97998895987
Hannah Woodward <hannah@woodward.sh>
2023-02-12 13:05:58 +0000

    Merge gitignore

---
+++ start.sh
@@ -0,0 +1,3 @@
+# docker run -i --rm --volume=${PWD}:/home/docker/jupyter-climate -w /home/docker/jupyter-climate -p 8888:8888 -t woodwardsh/jupyter-climate:latest
+
+docker run -i --rm --volume=${PWD}:/home/docker/jupyter-climate -w /home/docker/jupyter-climate -p 8888:8888 -t jupyter-climate
